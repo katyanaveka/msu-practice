453f2a5704a8aae12e8128a70043d6d02197e015
katyanaveka <50590208+katyanaveka@users.noreply.github.com>
2022-10-18 12:18:16 +0300

    Update setup.py

--- hw1/setup.py
+++ hw1/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='dgm_utils',
+    name='utils_and_cases',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
